669f39b3f24aebb2751533c16b470831ffde1dd7
Crowdbotics <team@crowdbotics.com>
2020-12-17 17:23:53 +0000

    Initial commit.

---
+++ backend/the_jumper_app_23425/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for the_jumper_app_23425 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_jumper_app_23425.settings')
+
+application = get_wsgi_application()
