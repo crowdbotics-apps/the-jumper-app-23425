8810f66c9699acc1ee51fd1013b9baac57bd9560
Crowdbotics <team@crowdbotics.com>
2020-12-19 17:00:02 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_jumper_app_23425.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_jumper_app_23425.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
